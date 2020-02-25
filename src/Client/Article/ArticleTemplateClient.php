9edd9aff9fa453d6e09c41816d44d4a48c3140d7
Sascha Nützel <nuetzel@sc-networks.com>
2020-02-25 16:06:52 +0100

    Add new ArticleTemplate Methods
    - fixes #81

--- src/Client/Article/ArticleTemplateClient.php
+++ src/Client/Article/ArticleTemplateClient.php
@@ -5,16 +5,48 @@ namespace Scn\EvalancheSoapApiConnector\Client\Article;
 use Scn\EvalancheSoapApiConnector\Client\AbstractClient;
 use Scn\EvalancheSoapApiConnector\Client\ClientInterface;
 use Scn\EvalancheSoapApiConnector\Client\Generic\ResourceTrait;
+use Scn\EvalancheSoapStruct\Struct\Generic\ResourceInformationInterface;
 
-/**
- * Class ArticleTemplateClient
- *
- * @package Scn\EvalancheSoapApiConnector\Client\Article
- */
 final class ArticleTemplateClient extends AbstractClient implements ArticleTemplateClientInterface
 {
     use ResourceTrait;
 
     const PORTNAME = 'articletemplate';
     const VERSION = ClientInterface::VERSION_V0;
+
+    public function create(
+        string $title,
+        int $typeId,
+        string $template,
+        int $folderId
+    ): ResourceInformationInterface {
+        return $this->responseMapper->getObject(
+            $this->soapClient->create(
+                [
+                    'title' => $title,
+                    'type_id' => $typeId,
+                    'template' => $template,
+                    'folder_id' => $folderId
+                ]
+            ),
+            'createResult',
+            $this->hydratorConfigFactory->createResourceInformationConfig()
+        );
+    }
+
+    public function updateTemplate(
+        int $templateId,
+        string $template
+    ): ResourceInformationInterface {
+        return $this->responseMapper->getObject(
+            $this->soapClient->updateTemplate(
+                [
+                    'template_id' => $templateId,
+                    'template' => $template
+                ]
+            ),
+            'updateTemplateResult',
+            $this->hydratorConfigFactory->createResourceInformationConfig()
+        );
+    }
 }
