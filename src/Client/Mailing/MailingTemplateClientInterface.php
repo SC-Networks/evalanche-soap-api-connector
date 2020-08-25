4d78df6f57993b33442bdb426bbe426dfb14d23d
Daniel Jakob <jakob@sc-networks.com>
2020-08-25 12:17:19 +0200

    Add MailingTemplate article management methods
    Fixes #113

--- src/Client/Mailing/MailingTemplateClientInterface.php
+++ src/Client/Mailing/MailingTemplateClientInterface.php
@@ -4,7 +4,9 @@ namespace Scn\EvalancheSoapApiConnector\Client\Mailing;
 
 use Scn\EvalancheSoapApiConnector\Client\ClientInterface;
 use Scn\EvalancheSoapApiConnector\Client\Generic\ResourceTraitInterface;
+use Scn\EvalancheSoapApiConnector\Exception\EmptyResultException;
 use Scn\EvalancheSoapStruct\Struct\Generic\ResourceInformationInterface;
+use Scn\EvalancheSoapStruct\Struct\Mailing\MailingArticleInterface;
 
 /**
  * Interface MailingTemplateClientInterface
@@ -21,4 +23,39 @@ interface MailingTemplateClientInterface extends ClientInterface, ResourceTraitI
      * @throws \Scn\EvalancheSoapApiConnector\Exception\EmptyResultException
      */
     public function updateTitle(int $id, string $title): ResourceInformationInterface;
+
+
+    /**
+     * @param int $id
+     * @param array $articles
+     *
+     * @return MailingArticleInterface[]
+     * @throws EmptyResultException
+     */
+    public function addArticles(int $id, array $articles): array;
+
+    /**
+     * @param int $id
+     *
+     * @return bool
+     * @throws EmptyResultException
+     */
+    public function removeAllArticles(int $id): bool;
+
+    /**
+     * @param int $id
+     * @param int[] $referenceIds
+     *
+     * @return MailingArticleInterface[]
+     * @throws EmptyResultException
+     */
+    public function removeArticles(int $id, array $referenceIds): array;
+
+    /**
+     * @param int $id
+     *
+     * @return MailingArticleInterface[]
+     * @throws EmptyResultException
+     */
+    public function getArticlesByMailingTemplateId(int $id): array;
 }
