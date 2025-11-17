58264de1a4f70983f2498881dc3f8b1265264b57
Daniel Jakob <jakob@sc-networks.com>
2025-11-17 08:34:24 +0100

    Add new leadpage-template methods
    This adds several methods for slot-manipulation in leadpage-templates

--- tests/Client/LeadPageTemplate/LeadpageTemplateClientTest.php
+++ tests/Client/LeadPageTemplate/LeadpageTemplateClientTest.php
@@ -11,13 +11,18 @@ use Scn\EvalancheSoapApiConnector\Extractor\ExtractorInterface;
 use Scn\EvalancheSoapApiConnector\Hydrator\Config\HydratorConfigFactoryInterface;
 use Scn\EvalancheSoapApiConnector\Hydrator\Config\HydratorConfigInterface;
 use Scn\EvalancheSoapApiConnector\Mapper\ResponseMapperInterface;
+use Scn\EvalancheSoapApiConnector\TestCase;
+use Scn\EvalancheSoapStruct\Struct\Generic\HashMapInterface;
 use Scn\EvalancheSoapStruct\Struct\Generic\ResourceInformationInterface;
 use Scn\EvalancheSoapStruct\Struct\LeadPage\LeadpageArticleInterface;
+use Scn\EvalancheSoapStruct\Struct\LeadPage\LeadpageSlotConfigurationInterface;
+use Scn\EvalancheSoapStruct\Struct\LeadPage\LeadpageSlotInterface;
+use Scn\EvalancheSoapStruct\Struct\LeadPage\LeadpageSlotItemInterface;
 use Scn\EvalancheSoapStruct\Struct\LeadPageTemplate\LeadpageTemplateConfigurationInterface;
 use Scn\EvalancheSoapStruct\Struct\LeadPageTemplate\TemplatesSourcesInterface;
-use \stdClass;
+use stdClass;
 
-class LeadpageTemplateClientTest extends \Scn\EvalancheSoapApiConnector\TestCase
+class LeadpageTemplateClientTest extends TestCase
 {
     use CommonResourceMethodsTestTrait;
     /**
@@ -61,6 +66,13 @@ class LeadpageTemplateClientTest extends \Scn\EvalancheSoapApiConnector\TestCase
             'removeArticles',
             'getArticles',
             'addArticles',
+            'removeSlot',
+            'removeTemplateFromSlot',
+            'getSlotConfiguration',
+            'addSlot',
+            'updateSlot',
+            'addTemplatesToSlot',
+            'updateSlotTemplates',
         ]);
         $this->responseMapper = $this->createMock(ResponseMapperInterface::class);
         $this->hydratorConfigFactory = $this->createMock(HydratorConfigFactoryInterface::class);
@@ -444,6 +456,290 @@ class LeadpageTemplateClientTest extends \Scn\EvalancheSoapApiConnector\TestCase
         $this->assertContainsOnlyInstancesOf(
             LeadpageArticleInterface::class,
             $this->subject->getArticlesByLeadpageTemplateId($id)
+        );
+    }
+
+    public function testRemoveSlotRemoves(): void
+    {
+        $id = 123;
+        $slotId = 666;
+
+        $response = new stdClass();
+        $response->removeSlotResult = true;
+
+        $this->soapClient->expects($this->once())
+            ->method('removeSlot')
+            ->with([
+                'leadpage_template_id' => $id,
+                'slot_id' => $slotId,
+            ])
+            ->willReturn($response);
+
+        $this->responseMapper->expects($this->once())
+            ->method('getBoolean')
+            ->willReturn($response->removeSlotResult);
+
+        $this->assertTrue($this->subject->removeSlot($id, $slotId));
+    }
+
+    public function testRemoveTemplateFromSlotRemoves(): void
+    {
+        $id = 123;
+        $slotId = 666;
+        $templateType = 444;
+        $articleTypeId = 333;
+
+        $response = new stdClass();
+        $response->removeTemplateFromSlotResult = true;
+
+        $this->soapClient->expects($this->once())
+            ->method('removeTemplateFromSlot')
+            ->with([
+                'leadpage_template_id' => $id,
+                'slot_id' => $slotId,
+                'template_type' => $templateType,
+                'article_type_id' => $articleTypeId
+            ])
+            ->willReturn($response);
+
+        $this->responseMapper->expects($this->once())
+            ->method('getBoolean')
+            ->willReturn($response->removeTemplateFromSlotResult);
+
+        $this->assertTrue(
+            $this->subject->removeTemplateFromSlot(
+                $id,
+                $slotId,
+                $templateType,
+                $articleTypeId
+            )
+        );
+    }
+
+    public function testGetSlotConfigurationReturnsResult(): void
+    {
+        $id = 1234;
+
+        $config = $this->createMock(HydratorConfigInterface::class);
+        $object = $this->createMock(LeadpageSlotConfigurationInterface::class);
+
+        $response = new stdClass();
+        $response->getSlotConfigurationResult = $object;
+
+        $this->hydratorConfigFactory->expects($this->once())
+            ->method('createLeadpageSlotConfigurationConfig')
+            ->willReturn($config);
+
+        $this->soapClient->expects($this->once())
+            ->method('getSlotConfiguration')
+            ->with([
+                'leadpage_template_id' => $id,
+            ])
+            ->willReturn($response);
+
+        $this->responseMapper->expects($this->once())
+            ->method('getObject')
+            ->with(
+                $response,
+                'getSlotConfigurationResult',
+                $config
+            )
+            ->willReturn($response->getSlotConfigurationResult);
+
+        self::assertSame(
+            $object,
+            $this->subject->getSlotConfiguration($id)
+        );
+    }
+
+    public function testAddSlotAddsSlots(): void
+    {
+        $id = 1234;
+        $slotNumber = 666;
+
+        $config = $this->createMock(HydratorConfigInterface::class);
+        $object = $this->createMock(LeadpageSlotInterface::class);
+
+        $response = new stdClass();
+        $response->addSlotResult = $object;
+
+        $this->hydratorConfigFactory->expects($this->once())
+            ->method('createLeadpageSlotConfig')
+            ->willReturn($config);
+
+        $this->soapClient->expects($this->once())
+            ->method('addSlot')
+            ->with([
+                'leadpage_template_id' => $id,
+                'slot_number' => $slotNumber
+            ])
+            ->willReturn($response);
+
+        $this->responseMapper->expects($this->once())
+            ->method('getObject')
+            ->with(
+                $response,
+                'addSlotResult',
+                $config
+            )
+            ->willReturn($response->addSlotResult);
+
+        self::assertSame(
+            $object,
+            $this->subject->addSlot($id, $slotNumber)
+        );
+    }
+
+    public function testUpdateSlotUpdatesSlot(): void
+    {
+        $id = 1234;
+        $slotNumber = 666;
+        $slotId = 555;
+        $name = 'some-name';
+        $sortTypeId = 444;
+        $sortValue = 333;
+
+        $config = $this->createMock(HydratorConfigInterface::class);
+        $object = $this->createMock(LeadpageSlotInterface::class);
+
+        $response = new stdClass();
+        $response->updateSlotResult = $object;
+
+        $this->hydratorConfigFactory->expects($this->once())
+            ->method('createLeadpageSlotConfig')
+            ->willReturn($config);
+
+        $this->soapClient->expects($this->once())
+            ->method('updateSlot')
+            ->with([
+                'leadpage_template_id' => $id,
+                'slot_id' => $slotId,
+                'slot_number' => $slotNumber,
+                'name' => $name,
+                'sort_type_id' => $sortTypeId,
+                'sort_value' => $sortValue
+            ])
+            ->willReturn($response);
+
+        $this->responseMapper->expects($this->once())
+            ->method('getObject')
+            ->with(
+                $response,
+                'updateSlotResult',
+                $config
+            )
+            ->willReturn($response->updateSlotResult);
+
+        self::assertSame(
+            $object,
+            $this->subject->updateSlot(
+                $id,
+                $slotId,
+                $slotNumber,
+                $name,
+                $sortTypeId,
+                $sortValue
+            )
+        );
+    }
+
+    public function testAddTemplatesToSlotAdds(): void
+    {
+        $id = 1234;
+        $slotId = 555;
+        $data = $this->createMock(HashMapInterface::class);
+        $articleTypeId = 666;
+        $allowedTemplates = [];
+
+        $config = $this->createMock(HydratorConfigInterface::class);
+        $object = $this->createMock(LeadpageSlotItemInterface::class);
+
+        $response = new stdClass();
+        $response->addTemplatesToSlotResult = $object;
+
+        $this->hydratorConfigFactory->expects($this->once())
+            ->method('createLeadpageSlotItemConfig')
+            ->willReturn($config);
+
+        $this->soapClient->expects($this->once())
+            ->method('addTemplatesToSlot')
+            ->with([
+                'leadpage_template_id' => $id,
+                'slot_id' => $slotId,
+                'data' => $data,
+                'article_type_id' => $articleTypeId,
+                'allowed_templates' => $allowedTemplates,
+            ])
+            ->willReturn($response);
+
+        $this->responseMapper->expects($this->once())
+            ->method('getObject')
+            ->with(
+                $response,
+                'addTemplatesToSlotResult',
+                $config
+            )
+            ->willReturn($response->addTemplatesToSlotResult);
+
+        self::assertSame(
+            $object,
+            $this->subject->addTemplatesToSlot(
+                $id,
+                $slotId,
+                $data,
+                $articleTypeId,
+                $allowedTemplates
+            )
+        );
+    }
+
+    public function testUpdateSlotTemplatesUpdates(): void
+    {
+        $id = 1234;
+        $slotId = 555;
+        $data = $this->createMock(HashMapInterface::class);
+        $articleTypeId = 666;
+        $allowedTemplates = [];
+
+        $config = $this->createMock(HydratorConfigInterface::class);
+        $object = $this->createMock(LeadpageSlotItemInterface::class);
+
+        $response = new stdClass();
+        $response->updateSlotTemplatesResult = $object;
+
+        $this->hydratorConfigFactory->expects($this->once())
+            ->method('createLeadpageSlotItemConfig')
+            ->willReturn($config);
+
+        $this->soapClient->expects($this->once())
+            ->method('updateSlotTemplates')
+            ->with([
+                'leadpage_template_id' => $id,
+                'slot_id' => $slotId,
+                'data' => $data,
+                'article_type_id' => $articleTypeId,
+                'allowed_templates' => $allowedTemplates,
+            ])
+            ->willReturn($response);
+
+        $this->responseMapper->expects($this->once())
+            ->method('getObject')
+            ->with(
+                $response,
+                'updateSlotTemplatesResult',
+                $config
+            )
+            ->willReturn($response->updateSlotTemplatesResult);
+
+        self::assertSame(
+            $object,
+            $this->subject->updateSlotTemplates(
+                $id,
+                $slotId,
+                $data,
+                $articleTypeId,
+                $allowedTemplates
+            )
         );
     }
 }
